01f704b184b103b94fac2ec15f7c9af65b3d1260
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-05-15 14:24:33 +0900

    add twitter and facebook login function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,51 @@
 class User < ActiveRecord::Base
+
+  has_many :favorites
+  has_many :posts
+  has_many :comments
+
+  mount_uploader :image, ImageUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter, :facebook]
+         :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter, :facebook]
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    if auth.provider == 'facebook'
+      unless user
+        user = User.new(
+          uid:      auth.uid,
+          provider: auth.provider,
+          username: auth.info.name,
+          email:    User.dummy_email(auth),
+          password: Devise.friendly_token[0, 20]
+        )
+        user.remote_image_url = auth.info.image.gsub('http://','https://')
+        user.save
+      end
+    elsif auth.provider == 'twitter'
+      unless user
+        user = User.new(
+          uid:      auth.uid,
+          provider: auth.provider,
+          username: auth.info.name,
+          email:    User.dummy_email(auth),
+          password: Devise.friendly_token[0, 20]
+        )
+        user.remote_image_url = auth.info.image.gsub('http://','https://')
+        user.save
+      end
+    end
+    user
+  end
+
+private
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.omniauth :facebook, Settings.facebook[:app_id], Settings.facebook[:app_secret], info_fields: 'name'
+  config.omniauth :twitter, Settings.twitter[:app_id], Settings.twitter[:app_secret]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
