b342633811d39de57f098b40c2c62cbf22cf91d2
櫻本静香 <shizuka.sakuramoto.0916@gmail.com>
2016-12-23 02:32:33 +0900

    fix typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Settings.aws.access_key_id
-    aws_secret_access_key: Settings.aws.secret_access_key
+    aws_access_key_id: Settings.aws.access_key_id,
+    aws_secret_access_key: Settings.aws.secret_access_key,
     region: 'us-east-1'
   }
 
